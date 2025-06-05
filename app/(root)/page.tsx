9bf44a9d96f9330e4b7e2302da88f21efc78275a
Ezeofor PraiseGod <abuchipraise@gmail.com>
2025-06-05 13:45:09 +0100

    fixed globals css and some minor updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,30 @@
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import React from 'react';
+
 const Home = () => {
-	return <div className='flex'>Home</div>;
+	const loggedIn = { firstName: 'Praise' };
+
+	return (
+		<section className='no-scrollbar home'>
+			<div className='no-scrollbar home-content'>
+				<header className='home-header'>
+					<HeaderBox
+						type='greeting'
+						title='Welcome'
+						user={loggedIn?.firstName || 'Guest'}
+						subtext='Access and manage your account and transactions efficiently'
+					/>
+
+					<TotalBalanceBox
+						accounts={[]}
+						totalBanks={1}
+						totalCurrentBalance={1250.35}
+					/>
+				</header>
+			</div>
+		</section>
+	);
 };
 
 export default Home;
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AnimatedCounter = () => {
+	return <div>AnimatedCounter</div>;
+};
+
+export default AnimatedCounter;
